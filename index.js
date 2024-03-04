d562f738a18ffed36c25fe69dae772803c237831
saini-manisha <manishasaini46160@gmail.com>
2024-03-04 18:54:01 +0530

    episode01

---
+++ index.js
@@ -0,0 +1,19 @@
+   
+const heading=React.createElement("div",{id:"parent"},
+[
+    React.createElement("div",{id:"child"},
+[
+React.createElement("h1",{},"i am h1 tag"),
+React.createElement("h1",{},"i am h1 tag")
+]
+),
+React.createElement("div",{id:"child"},
+[
+React.createElement("h1",{},"i am h1 tag"),
+React.createElement("h1",{},"i am h1 tag")
+]
+)
+]
+);
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading)
